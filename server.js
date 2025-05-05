6be1904897a0804f98c5cf645e05d9647a863e9c
Saloni Saroha <salonisaroha5@gmail.com>
2025-05-05 10:52:39 +0530

    added server connection

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoutes); // Authentication Routes
+app.use("/auth", authRoutes); 
 
-const PORT = process.env.PORT || 5000;
+const habitRoutes = require("./routes/habitRoutes");
+app.use("/habits", habitRoutes);
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
